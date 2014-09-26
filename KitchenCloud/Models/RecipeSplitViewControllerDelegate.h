b68a18c4c22982875f96ee5869a0acc8fc84c89a
Emery Clark <emdog4@Emerys-MacBook-Pro.local>
2014-09-26 16:24:53 -0400

    add files - initial commit

---
+++ KitchenCloud/Models/RecipeSplitViewControllerDelegate.h
@@ -0,0 +1,14 @@
+//
+//  RecipeSplitViewControllerDelegate.h
+//  RecipeHelper
+//
+//  Created by emdog4 on 7/24/14.
+//  Copyright (c) 2014 Emery Clark. All rights reserved.
+//
+
+@import UIKit;
+@import Foundation;
+
+@interface RecipeSplitViewControllerDelegate : NSObject <UISplitViewControllerDelegate>
+
+@end
---
+++ KitchenCloud/Supporting Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KitchenCloud
+//
+//  Created by Emery Clark on 9/26/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
